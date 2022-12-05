b3ace91626f86f709152bfabcbb6f91817e44b2c
Yaroslava-Vorobets <slavavor85@gmail.com>
2022-12-05 01:15:53 +0100

    add styles

--- src/components/StatisticsFeedback/Statistics.Styled.js
+++ src/components/StatisticsFeedback/Statistics.Styled.js
@@ -7,11 +7,12 @@ export const List = styled.ul`
 export const ListEl = styled.li`
     list-style: none;
     border-radius: 5px;
-    padding: 23px;     
+    padding: 15px;     
     display: flex;
     align-items: center;
     justify-content:space-between;
     background-color: #f5f6f8;
     border: 1px solid #a4a6a8;
-    margin-bottom: 10px;`  
+    margin-bottom: 10px;
+    width: 400px`  
 
